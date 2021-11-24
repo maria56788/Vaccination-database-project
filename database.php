ce19d857a74faa01dbbff3c98a98a7634d5e548d
SergioAAI <sergioa_anguloi@hotmail.com>
2021-11-24 14:51:32 -0500

    Database can now connect
    Database.php on the concordia Server now works with this code

--- database.php
+++ database.php
@@ -1,14 +1,24 @@
 <?php
 session_start();
-$server = 'cnc353.encs.concordia.ca:3306';
-$username = 'w_chitta';
+$host = 'cnc353.encs.concordia.ca';
+$username = 'cnc353_2';
 $password = 'cloudy56';
-$database = 'cnc353_2';
+$db = 'cnc353_2';
+$port = "3306";
+$charset = 'utf8mb4';
+
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
 
 try {
-    $conn = new PDO("mysql:host=$server,dbname=$database;", $username, $password);
+    $conn = new \PDO($dsn, $username, $password, $options);
 } catch (PDOException $e) {
-    die("Could not connect to". $e->getMessage());
+    die("Could not connect to lol". $e->getMessage());
 }
 
 ?>
