2d11cc625ae754450cb3c22703ff4e0a16ca6b56
sabrina-m-rahman <88601700+sabrina-m-rahman@users.noreply.github.com>
2021-11-25 16:45:25 -0500

    update publichealthcarewroker-index

--- publichealthworker/index.php
+++ publichealthworker/index.php
@@ -1,10 +1,9 @@
 <?php require_once '../database.php';
 
 if (isset($conn)) {
-    $statement = $conn->prepare('SELECT * FROM cnc353_2.publichealthworker ');
-    $statement->execute();
+$statement = $conn->prepare('SELECT * FROM cnc353_2.publichealthworker AS PublicHealthWorker');
+$statement->execute();
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,11 +11,15 @@ if (isset($conn)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <title>Document</title>
 </head>
 <body>
+
+
 <table>
-    <h1>List of publichealthworker</h1>
+    <h1>List of public health workers</h1>
     <a href="./create.php">Add a new health worker</a>
     <thead>
         <tr>
@@ -29,20 +32,21 @@ if (isset($conn)) {
         </tr>
     </thead>
     <tbody>
-    <?php while ($row =$statement->fetch(PDO::FETCH_ASSOC,PDO::FETCH_ORI_NEXT)){?>
+        <?php while ($row =$statement->fetch(PDO::FETCH_ASSOC,PDO::FETCH_ORI_NEXT)){?>
             <tr>
                 <td><?= $row["facilityID"]?></td>
                 <td><?= $row["personID"]?></td>
                 <td><?= $row["employeeID"]?></td>
                 <td><?= $row["hourlyRate"]?></td>
                 <td><?= $row["jobType"]?></td>
+                
                 <td>
                     <a href="./delete.php?employeeID=<?= $row["employeeID"]?>,?facilityID=<?= $row["facilityID"]?>">Delete</a>
                     <a href="./edit.php?employeeID=<?= $row["employeeID"]?>,?facilityID=<?= $row["facilityID"]?>">Edit</a>
                 </td>
-
-            </tr>
-        <?php } ?>
+                
+                </tr>
+    <?php } ?>
     </tbody>
 </table>
 <p>Choose the link to go back to the homepage</p>
