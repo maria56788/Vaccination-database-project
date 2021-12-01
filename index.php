5b860b11a5fff154d653f8531aa1a85652073310
SergioAAI <sergioa_anguloi@hotmail.com>
2021-12-01 18:10:54 -0500

    Added links to index.php

--- index.php
+++ index.php
@@ -4,13 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <title>Main Project</title>
 </head>
 <body>
     <h1>Welcome</h1>
     <p>Choose one of the following links to connect to</p>
     <a href="./publichealthworker/">publichealthworker</a>
+    <a href="./person/">person</a><br>
+    <a href="./booking/">Booking</a>
+    <a href="./groupage/">Group Age</a>
+    <br>
+    <a href="./infection_type/">Covid infection type</a>
     <a href="./person/">person</a>
+    <a href="./Provinces/">Provinces</a>
+    <a href="./publichealthworker/">Public Health Worker</a>
+    <br>
+    <a href="./query18/">Query 18</a>
+    <a href="./query19/">Query 19</a>
+    <a href="./query20">Search Person</a>
 
 
     
