b9c4612070b393e4b6a0dd3ac18c8ef5cc19d0a1
SergioAAI <sergioa_anguloi@hotmail.com>
2021-12-01 21:04:10 -0500

    Change main index.php

--- index.php
+++ index.php
@@ -12,17 +12,25 @@
     <h1>Welcome</h1>
     <p>Choose one of the following links to connect to</p>
     <a href="./publichealthworker/">publichealthworker</a>
-    <a href="./person/">person</a><br>
+    <br>
+    <a href="./person/">person</a>
+    /
     <a href="./booking/">Booking</a>
+    /
     <a href="./groupage/">Group Age</a>
     <br>
     <a href="./infection_type/">Covid infection type</a>
-    <a href="./person/">person</a>
+    /
     <a href="./Provinces/">Provinces</a>
+    /
     <a href="./publichealthworker/">Public Health Worker</a>
     <br>
+    <a href="./query15/">Query 15</a>
+    /
     <a href="./query18/">Query 18</a>
+    /
     <a href="./query19/">Query 19</a>
+    <br>
     <a href="./query20">Search Person</a>
 
 
