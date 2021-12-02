394c6731595cf2869651d6b5d04e757dc8db95f6
sabrina-m-rahman <88601700+sabrina-m-rahman@users.noreply.github.com>
2021-12-02 00:44:54 -0500

    query13

---
+++ query13/query.php
@@ -0,0 +1,83 @@
+<?php
+require_once "../database.php";
+try {
+
+
+    $facilityInfo = $facility->fetch();
+    $nurses = $conn->prepare("SELECT publichealthworker.employeeID, person.firstName,person.lastName,person.email, publichealthworker.hourlyRate 
+    FROM publichealthworker
+    inner join person on publichealthworker.personID=person.pID
+    where publichealthworker.employeeID NOT IN 
+        (SELECT p.pID
+        FROM publichealthworker
+        INNER JOIN person p on publichealthworker.personID = p.pID
+        INNER JOIN history h on publichealthworker.employeeID = h.employeeID AND publichealthworker.facilityID = h.facilityID
+        WHERE publichealthworker.exist = 1
+        AND publichealthworker.facilityID = :facilityID
+        AND publichealthworker.jobType = 'nurse')
+    AND publichealthworker.jobType = 'nurse';;
+");
+    $nurses->bindParam(":facilityID", $_POST["facilityID"]);
+    $nurses->bindParam(":date", $_POST["date"]);
+    $nurses->execute();
+
+
+}
+ catch (PDOException $e) {
+    $_SESSION['errorMSG'] = 'Generic Error Message';
+    header("Location: index.php");
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Person Information</title>
+</head>
+<body>
+<div class="Output">
+    <div class="row">
+        <div class="col s6">
+            <p>Health and Social Services</p>
+        </div>
+        <div class="col s6">
+            <p>Proof of Vaccination against COVID-19</p>
+        </div>
+    </div>
+    <hr>
+
+
+    </div>
+    <hr>
+    <p>Nurses:</p>
+    <?php foreach ($nurses
+
+                   as $nurse) { ?>
+        <div class="row">
+            <div class="col s6">
+                <p>Information: <?= $nurse["employeeID"]." ".$nurse["firstName"]." ".$nurse["lastName"]."".$nurse["hourlyWage"]." ".$nurse["lastName"]?></p>
+                
+            </div>
+        </div>
+        
+
+    <?php } ?>
+    <p>Appointments</p>
+    <?php foreach ($persons
+
+                   as $person) { ?>
+        <div class="row">
+            <div class="col s6">
+                <p>Name: <?= $person["firstName"]." ".$person["middleName"]." ".$person["lastName"]?></p>
+                <p>At: <?= $person["startTime"]?></p>
+            </div>
+        </div>
+    <?php } ?>
+</div>
+</body>
+</html>
